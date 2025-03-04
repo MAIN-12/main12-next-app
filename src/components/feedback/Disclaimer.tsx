5ff93be346890456b50bb745f6c27cf0ed8d1bd1
CHARLIE <jcarlosbotero1@outlook.com>
2025-03-04 10:14:54 -0500

    update

--- src/components/feedback/Disclaimer.tsx
+++ src/components/feedback/Disclaimer.tsx
@@ -1,13 +1,23 @@
+import { useTranslations } from "./locales/getTranslations";
+
 export const Disclaimer = () => {
+    const t = useTranslations("disclaimer")
+
     return (
         <p className="text-xs mt-6">
-            Some account and system information may be sent to&nbsp;
-            <a href="https://main12.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">Main 12 LLC</a>. 
-            This information helps us resolve issues and improve our services, subject to our&nbsp;
-            <a href="/privacy" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">Privacy Policy</a>&nbsp;
-            and&nbsp;
-            <a href="/terms" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">Terms of Service</a>.&nbsp;
-            We may contact you via email for further details or updates.
+            {t('text')} &nbsp;
+            <a href="https://main12.com" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">
+                {t('company')}
+            </a>.
+            {t('moreInfo')} &nbsp;
+            <a href="/privacy" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">
+                {t('privacyPolicy')}
+            </a>&nbsp;
+            {t('and')}&nbsp;
+            <a href="/terms" target="_blank" rel="noopener noreferrer" className="text-gray-500 font-bold">
+                {t('termsOfService')}
+            </a>.&nbsp;
+            {t('contactInfo')}
         </p>
-    )
-}
+    );
+};
